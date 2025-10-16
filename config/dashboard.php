bbd794a01e30a5110fac2301e5d0d68588f22635
oasim24 <oasim547@gmmail.com>
2025-10-17 04:18:21 +0600

    last update

---
+++ config/dashboard.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Softrang E-commerce Dashboard Configuration
+    |--------------------------------------------------------------------------
+    |
+    | You can define any default settings or options here. This file must
+    | always return an array.
+    |
+    */
+
+    'name' => 'Softrang E-Commerce Dashboard',
+    'version' => '1.0.1',
+
+    // Example settings
+    'theme' => 'default',
+    'widgets' => [
+        'sales_chart' => true,
+        'orders_summary' => true,
+        'user_stats' => true,
+    ],
+];
